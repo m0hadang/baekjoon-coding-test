f4c8eda7f517afe9b3cefd8a4e036e0f70102e88
daewook kim <mohadang@outlook.com>
2023-07-27 17:13:44 +0900

    [Bronze V] Title: A+B, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut inputs = String::new();
+    match io::stdin().read_line(&mut inputs) {
+        Ok(_) => {
+            let numbers: Vec<&str> = inputs.split_whitespace().collect();
+            let a_val = numbers[0].parse::<i32>().unwrap();
+            let b_val = numbers[1].parse::<i32>().unwrap();
+            println!("{}", a_val + b_val);
+        }
+        _ => {}
+    }
+}
